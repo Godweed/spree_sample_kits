fcfef21528e4d367cbe930e6175f914ca2df7bf1
JAKEN <jmatteson@southwestboulder.com>
2017-05-23 14:40:44 -0700

    Change ALLL the thingsgit add .!

--- db/migrate/20170523181642_add_can_sample_to_spree_products.rb
+++ db/migrate/20170523181642_add_can_sample_to_spree_products.rb
@@ -1,5 +1,5 @@
 class AddCanSampleToSpreeProducts < ActiveRecord::Migration
   def change
-    add_column :spree_products, :can_sample, :boolean, :default => true
+    add_column :spree_products, :can_sample, :boolean, :default => false
   end
 end
